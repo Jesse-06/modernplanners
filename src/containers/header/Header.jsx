0a4a9e4e3d4164c12f42c40d7d5b91c1972cadaf
Jesse Soto <gotosoto4@gmail.com>
2023-06-02 02:29:33 -0500

    header commit

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,30 +6,31 @@ import ai from '../../assets/ai.png';
 const Header = () => {
   return (
     <div className=" gpt3__header section__padding" id="home">
-      <div className=" gpt3__header-content"> 
+      <div className=" gpt3__header-content">
         <h1 className=" gradient__text"> Artisan Designer Notebooks and Planners</h1>
-        <p> One of a kind, hand designed, artisan planners and notebooks offered exclusively by Modern Planners. Our modern <a href="https://www.modernplanners.co/#brand">planners</a> and <a
-href="https://www.modernplanners.co/#blog">notebooks</a> make for the perfect moment to
-celebrate a special occasion, birthday, holiday or a wonderful addition to any workstation. Subscribe to our mailing list so you don't miss out on our latest arrivals and best selling
-notebooks & planners for your daily life!</p>
-        <div className=" gpt3__header-content__input"> 
-        <form action="https://formsubmit.co/contact@modernplanners.co" method="POST">
-        <input type="hidden" name="_subject" value="Modern Planners Submission!"/>
-        <input type="hidden" name="_captcha" value="false"/>
-        <input type="email" name= "email" placeholder="Your Email Address..." required/>
-        <button type="submit">Subscribe</button> 
-        </form>
+        <p> Each <a href="https://www.modernplanners.co/#brand">planner</a> and <a
+          href="https://www.modernplanners.co/#blog">notebook</a> in our collection is a work of art, created with passion and care. From elegant cover designs to thoughtfully designed interior layouts, our products are a true reflection of artistic craftsmanship. Whether you're a professional, student, or creative enthusiast, our planners and notebooks make the perfect gift to celebrate a special occasion, birthday, holiday, or simply as a wonderful addition to any workstation.<br />
+          <br />
+
+          By subscribing to our mailing list, you'll stay informed and never miss out on our latest arrivals and best-selling notebooks and planners. Be the first to explore our newest designs, limited editions, and exclusive offers. We'll keep you inspired with fresh ideas, productivity tips, and creative prompts to enhance your daily routines.One of a kind, hand designed, artisan planners and notebooks offered exclusively by Modern Planners.</p>
+        <div className=" gpt3__header-content__input">
+          <form action="https://formsubmit.co/contact@modernplanners.co" method="POST">
+            <input type="hidden" name="_subject" value="Modern Planners Submission!" />
+            <input type="hidden" name="_captcha" value="false" />
+            <input type="email" name="email" placeholder="Your Email Address..." required />
+            <button type="submit">Subscribe</button>
+          </form>
         </div>
         <div className="gpt3__header-content__people">
-          <img src={people} alt= "Shop sustainable planners"/>
+          <img src={people} alt="Shop sustainable planners" />
           <p>Planners & Notebooks...that plant trees!</p>
         </div>
       </div>
-      <div className="gpt3__header-image"> 
-      <a href="#brand">
-        <img src={ai} alt="Shop 2023 daily planner"/>
+      <div className="gpt3__header-image">
+        <a href="#brand">
+          <img src={ai} alt="Shop 2023 daily planner" />
         </a>
-        </div>
+      </div>
     </div>
   )
 }
